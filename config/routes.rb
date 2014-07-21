71811682dd8186ab8a8bb4333e53cb2f667f204a
Matt Larson <matthew.larson@flukenetworks.com>
2014-07-21 13:33:45 -0600

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,15 @@ SampleApp::Application.routes.draw do
 =end
 
   # Routes that are named.
-  resources :users
-  resources :sessions, only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :users do
+    # Creates /users/1/following and /users/1/followers
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
